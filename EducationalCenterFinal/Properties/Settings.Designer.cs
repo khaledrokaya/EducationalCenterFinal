35525ed47fd400bcb2ab7b7b5b664b179567d46d
khaledrokaya <mostloda14@gmail.com>
2024-10-31 04:01:57 +0300

    Set Dataset

--- EducationalCenterFinal/Properties/Settings.Designer.cs
+++ EducationalCenterFinal/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EducationalCenterFinal.Properties
-{
-
-
+namespace EducationalCenterFinal.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KHALEDROKAYA;Initial Catalog=Open_Source_Education_Center;Integrated " +
+            "Security=True;Encrypt=True;TrustServerCertificate=True")]
+        public string Open_Source_Education_CenterConnectionString {
+            get {
+                return ((string)(this["Open_Source_Education_CenterConnectionString"]));
             }
         }
     }
