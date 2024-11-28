3ba6cac13403a6496010e8816f05b29c4989b32a
Aya Ahmed <ayaaltaras526@gmail.com>
2024-11-28 23:36:12 +0200

    Features Of CourseManage

--- EducationalCenterFinal/Admin/CourseManage/CourseManageForm.cs
+++ EducationalCenterFinal/Admin/CourseManage/CourseManageForm.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace EducationalCenterFinal.Admin.CourseManage
 {
@@ -23,7 +24,9 @@ namespace EducationalCenterFinal.Admin.CourseManage
         public CourseManageForm()
         {
             InitializeComponent();
-
+            //ربط الاحداث 
+            this.txtSearch.Enter += new System.EventHandler(this.txtSearch_Enter);
+            this.txtSearch.Leave += new System.EventHandler(this.txtSearch_Leave);
             //Maximize window
             this.ClientSize = new System.Drawing.Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             this.MaximizeBox = false;
@@ -45,6 +48,30 @@ namespace EducationalCenterFinal.Admin.CourseManage
                 courseMenuItem.Click += (sender, e) => CourseMenuItem_Click(course.courseId, "admin");
                 manageToolStripMenuItem.DropDownItems.Add(courseMenuItem);
             }
+            //,تغيير لون الخلفية ,تغيير لون النص  
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(118, 41, 82);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Century Gothic Italic", 9, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle.Padding = new Padding(4);
+
+            //لتغيير اسماء الاعمدة عن الا موجودة في DataBase
+            dataGridView1.DataSource = dp.courses.ToList();
+            dataGridView1.Columns["courseId"].HeaderText = "ID";
+            dataGridView1.Columns["courseName"].HeaderText = "Name";
+            dataGridView1.Columns["Description"].HeaderText = "Description";
+            dataGridView1.Columns["WorkOn"].HeaderText = "Day Of Week";
+            //dataGridView1.Columns["price"].HeaderText = "Price";
+            dataGridView1.Columns["NoOfHours"].HeaderText = "Hour";
+            dataGridView1.Columns["teacherId"].HeaderText = "Teacher ID";
+
+            //لاخفاء اعمدة معينة
+            dataGridView1.Columns["beginning"].Visible = false;
+            dataGridView1.Columns["attendances"].Visible = false;
+            dataGridView1.Columns["teacher"].Visible = false;
+            dataGridView1.Columns["enrollments"].Visible = false;
+            dataGridView1.Columns["exams"].Visible = false;
+            dataGridView1.Columns["payments"].Visible = false;
         }
 
         private void CourseMenuItem_Click(int CourseId, string role)
@@ -99,5 +126,137 @@ namespace EducationalCenterFinal.Admin.CourseManage
             new StudentManageForm(role).Show();
             this.Hide();
         }
+
+        private void CourseManageForm_Load(object sender, EventArgs e)
+        {
+            
+            txtSearch.Text = "Search";
+            txtSearch.ForeColor = Color.Gray;
+        }
+
+        private void txtSearch_Leave(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "")
+            {
+                txtSearch.Text = "Search";
+                txtSearch.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtSearch_Enter(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "Search")
+            {
+                txtSearch.Text = "";
+                txtSearch.ForeColor = Color.Black;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "" || textBox8.Text == "" || textBox7.Text == "" || textBox5.Text == "" || textBox6.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Enter Data First.");
+                return;
+            }
+            addData();
+        }
+        private void addData()
+        {
+
+            cours c_add = new cours();
+            c_add.courseName = textBox2.Text;
+            c_add.Description = textBox8.Text;
+            c_add.WorkOn = textBox7.Text;
+            c_add.NoOfHours = int.Parse(textBox6.Text);
+            c_add.teacherId = int.Parse(textBox4.Text);
+            //c_add.price = int.Parse(textBox5.text);
+            dp.courses.Add(c_add);
+            dp.SaveChanges();
+
+            MessageBox.Show("Course Data Saved Successfully");
+            dataGridView1.DataSource = dp.courses.ToList();
+
+
+        }
+        private void resetForm()
+        {
+            textBox2.Text = "";
+            textBox8.Text = "";
+            textBox7.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox4.Text = "";
+            txtSearch.Text = "";
+            if (txtSearch.Text == "")
+            {
+                txtSearch.Text = "Search";
+                txtSearch.ForeColor = Color.Gray;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtSearch.Text, out int ID))
+            {
+                cours c = dp.courses.FirstOrDefault(x => x.courseId == ID);
+                c.courseName = textBox2.Text;
+                c.Description = textBox8.Text;
+                c.WorkOn = textBox7.Text;
+                //c.price= int.Parse(textBox5.Text);
+                c.NoOfHours = int.Parse(textBox6.Text);
+                c.teacherId = int.Parse(textBox4.Text);
+                dp.SaveChanges();
+                dataGridView1.DataSource = dp.courses.ToList();
+                MessageBox.Show("Data Is Edited");
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtSearch.Text))
+            {
+                cours c;
+                if (int.TryParse(txtSearch.Text, out int ID))
+                {
+                    c = dp.courses.FirstOrDefault(x => x.courseId == ID);
+                }
+                else
+                {
+                    string name = txtSearch.Text.ToLower();
+                    c = dp.courses.FirstOrDefault(x => x.courseName.ToLower().Contains(name));
+                }
+                if (c != null)
+                {
+                    textBox2.Text = c.courseName;
+                    textBox8.Text = c.Description;
+                    textBox7.Text = c.WorkOn;
+                    //textBox5.Text = c.price.ToString();
+                    textBox6.Text = c.NoOfHours.ToString();
+                    textBox4.Text = c.teacherId.ToString();
+                    button1.Enabled = false;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtSearch.Text, out int ID))
+            {
+                cours c = dp.courses.FirstOrDefault(x => x.courseId == ID);
+                dp.courses.Remove(c);
+                dp.SaveChanges();
+                dataGridView1.DataSource = dp.courses.ToList();
+                MessageBox.Show("Data Is Deleted");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            resetForm();
+            button1.Enabled = true;
+        }
     }
 }
+
