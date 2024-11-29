223bc31e4e141fa577c7bc158b9bb9445db63b06
khaledrokaya <mostloda14@gmail.com>
2024-11-29 06:30:59 +0200

    Add price to course in dataSet and Entity Model

--- EducationalCenterFinal/cours.cs
+++ EducationalCenterFinal/cours.cs
@@ -30,6 +30,7 @@ namespace EducationalCenterFinal
         public Nullable<System.TimeSpan> beginning { get; set; }
         public Nullable<decimal> NoOfHours { get; set; }
         public int teacherId { get; set; }
+        public Nullable<decimal> price { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<attendance> attendances { get; set; }
